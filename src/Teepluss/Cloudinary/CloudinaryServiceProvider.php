7a21d539f060b985dd51e293b2290d0c3c743fc0
Gavin Potts <gavin.potts@gmail.com>
2015-02-24 10:29:39 -0500

    Fix to directory

--- src/Teepluss/Cloudinary/CloudinaryServiceProvider.php
+++ src/Teepluss/Cloudinary/CloudinaryServiceProvider.php
@@ -19,7 +19,7 @@ class CloudinaryServiceProvider extends ServiceProvider {
 	public function boot()
 	{
 		$this->publishes([
-			__DIR__.'/../../../config/config.php'  => config_path('cloudinary.php'),
+			__DIR__.'/../../config/config.php'  => config_path('cloudinary.php'),
 		]);
 	}
 
